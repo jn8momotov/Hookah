27d7f08fa5212b4d11aee90c8e293d3315b22f1f
Evgeniy Momotov <jn8momotov@gmail.com>
2019-11-25 18:33:52 +0300

    [+] place model, realm service

--- HookahPlaces/Models/Place.swift
+++ HookahPlaces/Models/Place.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+import RealmSwift
+
+final class Place: Object {
+    @objc dynamic var id = UUID().uuidString
+    @objc dynamic var image: Data?
+    @objc dynamic var name = ""
+    @objc dynamic var phone = ""
+    
+    @objc dynamic var rating: Float = 0.0
+    @objc dynamic var ratingHookah: Float = 0.0
+    @objc dynamic var ratingPlace: Float = 0.0
+    @objc dynamic var ratingStaff: Float = 0.0
+    
+    @objc dynamic var metro = ""
+    @objc dynamic var address = ""
+    @objc dynamic var distanceTo: Float = 0.0
+    @objc dynamic var latitude: Double = 0.0
+    @objc dynamic var longitude: Double = 0.0
+    
+    @objc dynamic var bankCard = false
+    @objc dynamic var gameConsole = false
+    @objc dynamic var restarting = false
+    @objc dynamic var tableGames = false
+    @objc dynamic var theirAlko = false
+    @objc dynamic var theirFoot = false
+    @objc dynamic var theirDrink = false
+    @objc dynamic var wifi = false
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
--- HookahPlaces/Services/RealmService.swift
+++ HookahPlaces/Services/RealmService.swift
@@ -7,3 +7,60 @@
 //
 
 import Foundation
+import RealmSwift
+
+protocol RealmServiceProtocol {
+    func get<T: Object>(_ type: T.Type) -> [T]
+    func get<T: Object>(_ type: T.Type, predicate: NSPredicate) -> [T]
+    func getResults<T: Object>(_ type: T.Type, predicate: NSPredicate) -> Results<T>
+    func edit(_ handler: @escaping () -> Void)
+    func save(_ object: Object)
+    func delete(_ object: Object)
+    func deleteAll<T: Object>(_ type: T.Type)
+}
+
+final class RealmService: RealmServiceProtocol {
+    static let shared: RealmServiceProtocol = RealmService()
+    
+    private init() {
+        print("Realm url: \(String(describing: realm.configuration.fileURL))")
+    }
+    
+    private let realm = try! Realm()
+    
+    func get<T: Object>(_ type: T.Type) -> [T] {
+        return realm.objects(type).compactMap({ $0 })
+    }
+    
+    func get<T: Object>(_ type: T.Type, predicate: NSPredicate) -> [T] {
+        return realm.objects(type).filter(predicate).compactMap({ $0 })
+    }
+    
+    func getResults<T: Object>(_ type: T.Type, predicate: NSPredicate) -> Results<T> {
+        return realm.objects(type).filter(predicate)
+    }
+    
+    func edit(_ handler: @escaping () -> Void) {
+        try? realm.write {
+            handler()
+        }
+    }
+    
+    func save(_ object: Object) {
+        try? realm.write {
+            realm.add(object, update: .modified)
+        }
+    }
+    
+    func delete(_ object: Object) {
+        try? realm.write {
+            realm.delete(object)
+        }
+    }
+    
+    func deleteAll<T: Object>(_ type: T.Type) {
+        try? realm.write {
+            realm.delete(realm.objects(type))
+        }
+    }
+}
